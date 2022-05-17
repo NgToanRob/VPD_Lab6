8f48d55b5ea06930c9d7bdca3005588e6cbf3c1d
Toan <nguen>
2022-05-17 10:59:24 +0300

    edit 3line in task2

--- data/task2/Draw.m
+++ data/task2/Draw.m
@@ -104,7 +104,7 @@ plot3(line(:,1), line(:,2), line(:,3), 'b', 'LineWidth', 2);
 xlabel('X ($m$)','Interpreter','latex');
 ylabel('Y ($m$)','Interpreter','latex');
 zlabel('Z ($m$)','Interpreter','latex');
-legend 'Target 1 (-10, 10, 10)' 'Target 2 (-10, 10, 10)' 'Target 3 (-10, 10, 20)';
+legend 'Target 1 (-10, 10, 10)' 'Target 2 (-10, -10, 10)' 'Target 3 (-10, 10, 20)';
 text(line(end,1), line(end,2), line(end,3), 'Target 3');
 grif on;
 hold on;
